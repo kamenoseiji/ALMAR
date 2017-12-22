25ef50983f84458cf912849535495cbd45a0f25f
kamenoseiji <seiji.kameno@nao.ac.jp>
2017-12-22 14:00:59 -0300

    modified:   PlotFlux.R

--- PlotFlux.R
+++ PlotFlux.R
@@ -1,4 +1,6 @@
 library(xtable)
+library(plotly)
+library(htmlwidgets)
 
 #-------- Load Flux.Rdata from web
 load(url("http://www.alma.cl/~skameno/Grid/Stokes/Flux.Rdata"))     # Data frame of FLDF
@@ -30,49 +32,34 @@ for(freq_index in 1:numFreq){
 	polDF <- data.frame( Src=as.character(sourceList), numObs=numObs, I=medI, eI = eI, Q=medQ, eQ = eQ, U=medU, eU = eU, P=sqrt(medQ^2 + medU^2), eP=sqrt(eQ^2 + eU^2)/medI, p=100.0*sqrt(medQ^2 + medU^2)/medI, EVPA=90.0*atan2(medU, medQ)/pi )
 	polDF <- polDF[order(polDF$P, decreasing=T),]
 	rownames(polDF) <- c(1:nrow(polDF))
-	cat(sprintf('Frequency %.1f GHz : 60-day median by %s\n', freqList[freq_index], as.character(Today)))
+	CaptionText <- paste("<p>", sprintf('Frequency %.1f GHz : 60-day median as of %s\n', freqList[freq_index], as.character(Today)), "</p>", sep='\n')
+	cat(sprintf('Frequency %.1f GHz : 60-day median as of %s\n', freqList[freq_index], as.character(Today)))
 	cat('Source       #obs   I [Jy]   Q [Jy]   U [Jy]    %Pol  EVPA [deg]\n')
 	for(index in 1:numSrc){
 		pDF <- polDF[index,]
 		cat(sprintf("%10s   (%2d)   %6.2f   %6.2f   %6.2f   %6.1f    %6.1f\n", pDF$Src, pDF$numObs, pDF$I, pDF$Q, pDF$U, pDF$p, pDF$EVPA))
 	}
+	names(polDF) <- c('Source', '#obs', 'I (Jy)', 'err I', 'Q (Jy)', 'err Q', 'U (Jy)', 'err U', 'P (Jy)', 'err P', '%pol', 'EVPA (deg)')
+	polDF$Source <- paste('<a href="', polDF$Source, '.flux.html" target="_new" >', polDF$Source, ' </a>', sep='')
+	htmlFile <- sprintf('Stokes%.0fGHz.html', freqList[freq_index])
+	html.head <- paste("<head>", '<link rel="stylesheet" type="text/css" href="http://www.alma.cl/~skameno/resources/amapola.css" />', "</head>", sep='\n')
+	html.table <- paste(print(xtable(polDF, digits=c(0,0,0,3,3,3,3,3,3,3,3,1,1)), include.rownames=F, type="html", sanitize.text.function=function(x){x}, htmlFile), collapse="\n")
+	html.body <- paste("<body>", CaptionText, html.table, "</body>")
+	write(paste(html.head, html.body, sep='\n'), htmlFile)
 }
 
-if(0){
-	
-#for(src_index in 1:numSrc){
-#	cat(sprintf("%10s  %5.1f  %6.3f  %6.3f\n", polDF$Src[src_index], polDF$I[src_index], polDF$Q[src_index]/polDF$I[src_index], polDF$U[src_index]/polDF#$I[src_index]))
-#}
-
-# print(xtable(polDF, digits=3), include.rownames=F, type = "html")
-
-pdf(sprintf('Flux-%s.pdf', BandName), width=8, height=11)
-par.old <- par(no.readonly=TRUE)
-par(mfrow=c(3,1), oma=c(0, 0, 4, 0), mar=c(4,4,4,4))
-cat('Source     I [Jy]   Q [Jy]   U [Jy]   %Pol   EVPA [deg]\n')
-for(source in sourceList){
-	DF <- FLDF[FLDF$Src == source,]
-	pDF <- polDF[polDF$Src == source,]
-	cat(sprintf("%10s %6.2f %6.2f %6.2f %6.2f %6.2f\n", pDF$Src, pDF$I, pDF$Q, pDF$U, pDF$p, pDF$EVPA))
-	#-------- Plot Stokes I
-	plot(DF$Date, DF$I, type='n', xlab='Date', ylab='Stokes I [Jy]', main='Total Flux Density', ylim=c(0, 1.2*max(DF$I)))
-	color_vector <- rep("#000000FF", length(DF$Date))
-	color_vector[which(DF$flagNum > 0)] <- "#0000FF3F"
-	arrows(as.numeric(DF$Date), DF$I - DF$eI, as.numeric(DF$Date), DF$I + DF$eI, angle=90, length=0.0, col=color_vector)
-	points(DF$Date, DF$I, pch=20, col=color_vector)
-	
-	#-------- Plot polarized flux
-	plot(DF$Date, DF$P, type='n', xlab='Date', ylab='Polarized Flux [Jy]', main='Polarized Flux Density', ylim=c(0, 1.2* max(DF$P)))
-	arrows(as.numeric(DF$Date), DF$P - DF$eP, as.numeric(DF$Date), DF$P + DF$eP, angle=90, length=0.0, col=color_vector)
-	points(DF$Date, DF$P, pch=20, col=color_vector)
-	
-	#-------- Plot EVPA
-	plot(DF$Date, DF$EVPA, type='n', xlab='Date', ylab='EVPA [deg]', main='Polarization Angle', ylim=c(-90,90))
-	abline(h=90, lwd=0.1); abline(h=-90, lwd=0.1)
-	arrows(as.numeric(DF$Date), 180*(DF$EVPA - DF$eEVPA)/pi, as.numeric(DF$Date), 180*(DF$EVPA + DF$eEVPA)/pi, angle=90, length=0.0, col=color_vector)
-	points(DF$Date, DF$EVPA*180/pi, pch=20, col=color_vector)
-	mtext(side = 3, line=1, outer=T, text = sprintf('%s %s', source, BandName), cex=2)
+for(src_index in 1:numSrc){
+	DF <- FLDF[FLDF$Src == sourceList[src_index],]
+	DF$Freq <- paste(as.character(DF$Freq), "GHz")
+	dataRange <- matrix(c(1.1, -0.1, -0.1, 1.1), nrow=2) %*% c(min(DF$Date), max(DF$Date))
+	plot_I <- plot_ly(DF, x=~Date, y=~I, type="scatter", mode="markers", error_y = ~list(array=eI, thickness=1, width=0), color=~Freq, showlegend=F)
+	plot_I <- layout(plot_I, xaxis=list(showgrid=T, title='Date', range=c(min(DF$Date)-86400, max(DF$Date)+86400)), yaxis=list(showgrid=T, title='Stokes I [Jy]',rangemode='tozero'), title=sourceList[src_index])
+	plot_P <- plot_ly(DF, x=~Date, y=~P, type="scatter", mode="markers", error_y = ~list(array=eP, thickness=1, width=0), color=~Freq, showlegend=F)
+	plot_P <- layout(plot_P, xaxis=list(showgrid=T, title='Date', range=c(min(DF$Date)-86400, max(DF$Date)+86400)), yaxis=list(showgrid=T, title='Polarized Flux [Jy]',rangemode='tozero'))
+	plot_A <- plot_ly(DF, x=~Date, y=~EVPA*180/pi, type="scatter", mode="markers", error_y = ~list(array=eEVPA*180/pi, thickness=1, width=0), color=~Freq, showlegend=T)
+	plot_A <- layout(plot_A, xaxis=list(showgrid=T, title='Date', range=c(min(DF$Date)-86400, max(DF$Date)+86400)), yaxis=list(showgrid=T, title='EVPA [deg]',range=c(-91,91)))
+	allPlot <- subplot(plot_I, plot_P, plot_A, nrows=3, shareX=F, titleY=T)
+	htmlFile <- sprintf("%s.flux.html", sourceList[src_index])
+	htmlwidgets::saveWidget(allPlot, htmlFile)
 }
-dev.off()
-par(par.old)
-}
+
