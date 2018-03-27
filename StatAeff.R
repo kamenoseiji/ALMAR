862ded64e0d09a4c536ab2f3eab486b840b6ae43
SeijiKameno <kameno@ainodake.local>
2018-03-27 20:21:00 -0300

    new file:   ReadAeff.R 	modified:   StatAeff.R

--- StatAeff.R
+++ StatAeff.R
@@ -6,46 +6,33 @@ AeDF$Ae <- 0.5*(AeDF$AeX + AeDF$AeY)
 antList <- unique(AeDF$Ant)
 antList <- sort(as.character(antList[ grep('[C|P]', antList) ]))
 antNum <- length(antList)
-AeX_mean <- AeY_mean <- AeX_e <- AeY_e <- AeX_sd <- AeY_sd <- AeD_mean <- AeD_e <- AeD_sd <- AeN_mean <- AeN_e <- AeN_sd <-numeric(0)
+AeX_mean <- AeY_mean <- AeX_sd <- AeY_sd <- AeD_mean <- AeD_sd <- AeN_mean <- AeN_sd <-numeric(0)
 for(ant_index in 1:antNum){
-	DF <- AeDF[ ((AeDF$Ant == antList[ant_index]) & (AeDF$EL >= 40.0)),]
-	weightX <- weightY <- rep(1.0, length(DF$Ant))
+	DF <- AeDF[ (AeDF$Ant == antList[ant_index] ),]
 	AeX_median <- median(DF$AeX); AeY_median <- median(DF$AeY)
-	flag_X <- which( abs(DF$AeX - AeX_median) > 15.0 ); flag_Y <- which( abs(DF$AeY - AeY_median) > 15.0 )
-	weightX[flag_X] <- 0.00; weightY[flag_Y] <- 0.00
-	EL60 <- DF$EL - 60.0
-	Xfit <- lm(DF$AeX ~ EL60, weights=weightX); Yfit <- lm(DF$AeY ~ EL60, weights=weightY)
-	AeX_mean[ant_index] <- summary(Xfit)[['coefficients']][1,1]
-	AeX_e[ant_index] <- summary(Xfit)[['coefficients']][1,2]
-	AeX_sd[ant_index] <- sd(DF$AeX[which(weightX > 0.0)])
-	AeY_mean[ant_index] <- summary(Yfit)[['coefficients']][1,1]
-	AeY_e[ant_index] <- summary(Yfit)[['coefficients']][1,2]
-	AeY_sd[ant_index] <- sd(DF$AeY[which(weightY > 0.0)])
+	flag <- which( (abs(DF$AeX - AeX_median) < 15.0) |(abs(DF$AeY - AeY_median) < 15.0 ) )
+    DF <- DF[flag,]
+	AeX_mean[ant_index] <- quantile(DF$AeX, 0.75)
+	AeY_mean[ant_index] <- quantile(DF$AeY, 0.75)
+	AeX_sd[ant_index] <- sd(DF$AeX)
+	AeY_sd[ant_index] <- sd(DF$AeY)
 	#
 	DayDF <- DF[((DF$sunset < 2.5) | (DF$sunset > 18)),]
 	NgtDF <- DF[((DF$sunset > 2.5) & (DF$sunset < 18)),]
-	weightD <- rep(1.0, length(DayDF$Ant)); weightN <- rep(1.0, length(NgtDF$Ant))
-	AeD_median <- median(DayDF$Ae); AeN_median <- median(NgtDF$Ae)
-	flag_D <- which( abs(DayDF$Ae - AeD_median) > 15.0 ); flag_N <- which( abs(NgtDF$Ae - AeN_median) > 15.0 )
-	weightD[flag_D] <- 0.00; weightN[flag_N] <- 0.00
-	EL60 <- DayDF$EL - 60.0; Dfit <- lm(DayDF$Ae ~ EL60, weights=weightD)
-	EL60 <- NgtDF$EL - 60.0; Nfit <- lm(NgtDF$Ae ~ EL60, weights=weightN);
-	AeD_mean[ant_index] <- summary(Dfit)[['coefficients']][1,1]
-	AeD_e[ant_index] <- summary(Dfit)[['coefficients']][1,2]
-	AeD_sd[ant_index] <- sd(DayDF$Ae[which(weightD > 0.0)])
-	AeN_mean[ant_index] <- summary(Nfit)[['coefficients']][1,1]
-	AeN_e[ant_index] <- summary(Nfit)[['coefficients']][1,2]
-	AeN_sd[ant_index] <- sd(NgtDF$Ae[which(weightN > 0.0)])
+	AeD_mean[ant_index] <- quantile(DayDF$Ae, 0.75)
+	AeN_mean[ant_index] <- quantile(NgtDF$Ae, 0.75)
+	AeD_sd[ant_index] <- sd(DayDF$Ae)
+	AeN_sd[ant_index] <- sd(NgtDF$Ae) 
 }
-AeStatDF <- data.frame(Ant=antList, AeXm = AeX_mean, AeXe = AeX_e, AeXs = AeX_sd, AeYm = AeY_mean, AeYe = AeY_e, AeYs = AeY_sd, AeDm = AeD_mean, AeDe = AeD_e, AeDs = AeD_sd, AeNm = AeN_mean, AeNe = AeN_e, AeNs = AeN_sd)
+AeStatDF <- data.frame(Ant=antList, AeXm = AeX_mean, AeXs = AeX_sd, AeYm = AeY_mean, AeYs = AeY_sd, AeDm = AeD_mean, AeDs = AeD_sd, AeNm = AeN_mean, AeNs = AeN_sd)
 print(xtable(AeStatDF), include.rownames=F)
 
 pdf('AeStat.pdf')
 DayNight <- matrix(c(AeD_mean, AeN_mean - AeD_mean), ncol=2); rownames(DayNight) <- antList; colnames(DayNight) <- c('Night', 'Day')
 DayNight <- t(DayNight)
-barplot(DayNight, las=2, ylim=c(0,70), ylab='Efficiency [%]', main='Band 6 Day/Night Difference')
+barplot(DayNight, las=2, ylim=c(0,70), ylab='Efficiency [%]', main='Band 3 Day/Night Difference')
 xaxis <- (0.2 + 1) * 1:16 - 1/2
-arrows( xaxis-0.1, AeD_mean + AeD_e, xaxis-0.1, AeD_mean - AeD_e, length=0)
-arrows( xaxis+0.1, AeN_mean + AeN_e, xaxis+0.1, AeN_mean - AeN_e, length=0)
+arrows( xaxis-0.1, AeD_mean + AeD_sd, xaxis-0.1, AeD_mean - AeD_sd, length=0)
+arrows( xaxis+0.1, AeN_mean + AeN_sd, xaxis+0.1, AeN_mean - AeN_sd, length=0)
 text(5,65,sprintf('Night - Day = %.1f +- %.1f%%', mean(DayNight['Day',], na.rm=T), sd(DayNight['Day',], na.rm=T)))
 dev.off()
