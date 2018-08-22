f733f75e71cbebac2d2b46b7f3b5f31bc9b45730
Seiji Kameno <seiji.kameno@nao.ac.jp>
2018-08-22 15:39:51 -0300

    modified:   PlotFlux.R

--- PlotFlux.R
+++ PlotFlux.R
@@ -118,16 +118,18 @@ for(freq_index in 1:numFreq){
 for(src_index in 1:numSrc){
     rm(DF)
 	DF <- FLDF[FLDF$Src == sourceList[src_index],]
-	DF$Freq <- paste(as.character(DF$Freq), "GHz", sep=' ')
+	DF$FreqI <- sprintf("I %.1f GHz", DF$Freq)
+	DF$FreqP <- sprintf("P %.1f GHz", DF$Freq)
+	DF$FreqA <- sprintf("EVPA %.1f GHz", DF$Freq)
 	DF$Date <- as.POSIXct(DF$Date)
     DF <- DF[order(DF$Freq),]; rownames(DF) <- c(1:nrow(DF))
-	plot_I <- plot_ly(DF, x=~Date, y=~I, type="scatter", mode="markers", error_y = list(type="data", symmatric=T, array=~eI, thickness=1, width=0), color=~Freq, showlegend=F)
+	plot_I <- plot_ly(DF, x=~Date, y=~I, type="scatter", mode="markers", error_y = list(type="data", symmatric=T, array=~eI, thickness=1, width=0), color=~FreqI, showlegend=T)
 	plot_I <- layout(plot_I, xaxis=list(showgrid=T, title='Date', range=c(min(DF$Date)-86400, max(DF$Date)+86400)), yaxis=list(showgrid=T, title='Stokes I [Jy]',rangemode='tozero'), title=sourceList[src_index])
-	plot_P <- plot_ly(DF, x=~Date, y=~P, type="scatter", mode="markers", error_y = list(type="data", symmetric=F, array=~errU, arrayminus=~errL, thickness=1, width=0), color=~Freq, showlegend=F)
+	plot_P <- plot_ly(DF, x=~Date, y=~P, type="scatter", mode="markers", error_y = list(type="data", symmetric=F, array=~errU, arrayminus=~errL, thickness=1, width=0), color=~FreqP, showlegend=T)
 	plot_P <- layout(plot_P, xaxis=list(showgrid=T, title='Date', range=c(min(DF$Date)-86400, max(DF$Date)+86400)), yaxis=list(showgrid=T, title='Polarized Flux [Jy]',rangemode='tozero'))
-	plot_A <- plot_ly(DF, x=~Date, y=~EVPA*180/pi, type="scatter", mode="markers", error_y = list(type="data", array=~eEVPA*180/pi, thickness=1, width=0), color=~Freq, showlegend=T)
+	plot_A <- plot_ly(DF, x=~Date, y=~EVPA*180/pi, type="scatter", mode="markers", error_y = list(type="data", array=~eEVPA*180/pi, thickness=1, width=0), color=~FreqA, showlegend=T)
 	plot_A <- layout(plot_A, xaxis=list(showgrid=T, title='Date', range=c(min(DF$Date)-86400, max(DF$Date)+86400)), yaxis=list(showgrid=T, title='EVPA [deg]',range=c(-91,91)))
-	allPlot <- subplot(plot_I, plot_P, plot_A, nrows=3, shareX=F, titleY=T)
+	allPlot <- subplot(plot_I, plot_P, plot_A, nrows=3, shareX=T, titleY=T)
 	htmlFile <- sprintf("%s.flux.html", sourceList[src_index])
 	htmlwidgets::saveWidget(allPlot, htmlFile)
 }
