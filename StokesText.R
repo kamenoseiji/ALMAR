6b25491a9ab1b8473cd422a3ca58bf55e284b8c9
kamenoseiji <seiji.kameno@alma.cl>
2024-06-13 22:27:52 +0000

    modified:   StokesText.R

--- StokesText.R
+++ StokesText.R
@@ -1,11 +1,13 @@
+library(RColorBrewer)
+library(plotly, warn.conflicts=FALSE)
 library(parallel)   # multicore parallelization
 #library(doParallel) # multicore parallelization
 library(VGAM)       # for Rice distribution
 Sys.setenv(TZ="UTC")
+ALMA_lat <- -23.029 * pi / 180
 #sysIerr <- 0.005       # temporal Stokes I systematic error
 sysPerr <- 0.003       # temporal polarization systematic error
 minAntNum <- 5		   # Minimum number of antennas
-#numSubBand = c(3,3,3,1,1,1,1,1,1,1) # Number of sub-bands for Band[1-10]
 standardFreq <- list(40.0, 80.0, c(91.5,97.5,103.5), 154.9, 183.0, 233.0, 343.4, 410.2)
 numCore = detectCores()
 #-------- Functions
@@ -31,13 +33,6 @@ predStokes <- function(df){
     pred <- as.numeric(predict(fitV, newDF, interval='confidence', level=0.67)); newDF$V <- matrix(pred, ncol=3)[,1]; newDF$eV <- 0.5*(matrix(pred, ncol=3)[,3] - matrix(pred, ncol=3)[,2])
     newDF$Date <- df$Date[1]
     newDF$File <- df$File[1]
-    #newDF$P <- sqrt(df$Q^2 + df$U^2)
-    #sigmaSQ <- sqrt(df$eQ * df$eU + (df$I* sysPerr)^2)
-    #newDF$eP_lower <- qrice(0.15, sigmaSQ, FLDF$P)
-    #newDF[newDF$P < sigmaSQ,]$eP_lower <- 0.0
-    #newDF$eP_upper <- qrice(0.85, sigmaSQ, newDF$P)
-    #newDF$EVPA <- 0.5* atan2(df$U, df$Q)
-    #newDF$eEVPA <- 0.5* sqrt(newDF$Q^2 * newDF$eU^2 + newDF$U^2 * newDF$eQ^2) / (newDF$P)^2
     return(newDF)
 }
 #-------- Text Formatting
@@ -45,9 +40,14 @@ textResult <- function(entry){
     text_sd <- sprintf()
     return(text_sd)
 }
-
+getBand <- function(fileList){
+    return(as.integer(mclapply(fileList, function(fileName){
+            bandPointer <- as.integer(regexpr("RB_[0-10]", fileName)[1])
+            return(as.integer(substr(fileName, bandPointer+3, bandPointer+4)))
+        },mc.cores=numCore)))
+}
+#-------- Starging Process
 load('Flux.Rdata')
-FLDF$Band <- getBand(FLDF$File)
 recNum <- nrow(FLDF)
 FLDF <- FLDF[order(FLDF$Date, FLDF$Src, FLDF$Band, FLDF$Freq),]
 sliceDF <- function(slice){ return(FLDF[slice[1]:slice[2],])}
@@ -59,158 +59,44 @@ recMat <- matrix(c(recBorder,   c(tail(recBorder, n=length(recBorder)-1)-1, recN
 tempDF <- apply(recMat, 1, sliceDF)
 result <- mclapply(tempDF, predStokes, mc.cores=numCore)
 textDF <- do.call("rbind", result)
-
-
-
-
-
-FLDF[
-
-
-recLength <- diff(c(recBorder, recNum+1) )
-
-
-
-
-
-recBorder <- which((FLDF[index, ]$Src[1:(length(index)-1)] != FLDF[index, ]$Src[2:length(index)]) || (FLDF[index, ]$Date[1:(length(index)-1)] != FLDF[index, ]$Date[2:length(index)]))
-
-
-
-
-
-
-
-
-
-
-#srcDfList <- mclapply(unique(FLDF$Src), function(source){ return(FLDF[FLDF$Src == source,]) })
-srcDfList <- mclapply(unique(FLDF$Src), srcDfFilter)
-#scanDFList <- mclapply(unique(FLDF$Date), scanDfFilter)
-
-cores <- detectCores()
-cl <- makcCluster(cores)
-registerDoParallel(cl)
-
-srcNum <- length(srcDfList)
-result <- foreach(src_index = 1:srcNum) %dopar% {
-    cat(sprintf('%d : %s %d\n', src_index, srcDfList[[src_index]]$Src[1], length(unique(srcDfList[[src_index]]$Date))))
-    scanDfList <- mclapply(unique(srcDfList[[src_index]]$Date), scanDfFilter)
-}
-
-for(src_index in 1:srcNum){
-    srcDF <- srcDfList[[src_index]]
-    cat(sprintf('%d : %s %d\n', src_index, srcDF$Src[1], length(unique(srcDF$Date))))
-    scanDfList <- mclapply(unique(srcDF$Date), scanDfFilter)
-}
-    #tempList <- mclapply( scanDfList, predStokes )
-    
-
-for(srcDF in srcDfList){
-    cat(srcDF$Src[1])
-    scanList <- unique(srcDF$Date)
-    scanDfList <- mclapply(scanList, function(scan){ return(srcDF[srcDF$Date == scan,]) })
-    tempList <- mclapply( scanDfList, predStokes )
-}
-
-
-
-scanDFList <- mclapply(unique(FLDF$Date), function(scan){ return(FLDF[FLDF$Date == scan,])})
-
-
-
-tempList <- mclapply( srcDFList, function(srcDF){
-    scanDFList <- mclapply( unique(srcDF$Date), function(scan){ return(srcDF[srcDF$Date == scan,])})
-    return(mclapply(scanDfList, predStokes ))
-})
-    
-
-
-
-tempList <- mclapply( scanDfList, predStokes )
-    
-
-return(srcD)
-
-
-
-srcDfList <- list()
-
-
-
-for(source in sourceList){ srcDfList <- c(srcDfList, list(FLDF[FLDF$Src == source,]))}
-
-
-
-srcDF <- srcDfList[[1]]
-UniqueDate <- unique(srcDF$Date)
-scanDfList <- list()
-for(scan in UniqueDate){
-    scanDfList <- c(scanDfList, list(srcDF[srcDF$Date == scan,]))
-}
-tempList <- mclapply( scanDfList, predStokes )
-
-
-        
-
-
-
-FLDF$P <- sqrt(FLDF$Q^2 + FLDF$U^2)
-sigmaSQ <- sqrt(FLDF$eQ * FLDF$eU + (FLDF$I* sysPerr)^2)
-FLDF$eP_lower <- qrice(0.15, sigmaSQ, FLDF$P)
-FLDF[FLDF$P < sigmaSQ,]$eP_lower <- 0.0
-FLDF$eP_upper <- qrice(0.85, sigmaSQ, FLDF$P)
-FLDF$EVPA <- 0.5* atan2(FLDF$U, FLDF$Q)
-FLDF$eEVPA <- 0.5* sqrt(FLDF$Q^2 * FLDF$eU^2 + FLDF$U^2 * FLDF$eQ^2) / (FLDF$P)^2
+textDF <- na.omit(textDF)
+textDF$P <- sqrt(textDF$Q^2 + textDF$U^2)
+sigmaSQ <- sqrt(textDF$eQ * textDF$eU + (textDF$I* sysPerr)^2)
+textDF$eP_lower <- qrice(0.15, sigmaSQ, textDF$P)
+textDF[textDF$P < sigmaSQ,]$eP_lower <- 0.0
+textDF$eP_upper <- qrice(0.85, sigmaSQ, textDF$P)
+textDF$EVPA <- 0.5* atan2(textDF$U, textDF$Q)
+textDF$eEVPA <- 0.5* sqrt(textDF$Q^2 * textDF$eU^2 + textDF$U^2 * textDF$eQ^2) / (textDF$P)^2
 #---- Output to text data
-
-
-TextDF <- FLDF[order(FLDF$Date),]
-index <- which(abs(TextDF$Freq - 97.45) < 1.0)
-TextDF <- TextDF[-index,]
-TextDF$Src <- sprintf('%10s ', TextDF$Src)
-write.table(format(TextDF, digits=4), 'amapola.txt', sep='\t', quote=F, col.names=T, row.names=F)
-#
-#-------- Source List
-sourceList <- unique(FLDF$Src)
-numSrc <- length(sourceList)
-
-medI <- medQ <- medU <- eI <- eQ <- eU <- numeric(0)
-for(src_index in 1:numSrc){
-	DF <- FLDF[FLDF$Src == sourceList[src_index],]
-	medI[src_index] <- median(DF$I); medQ[src_index] <- median(DF$Q); medU[src_index] <- median(DF$U)
-	if(length(DF$eI) == 1){
-		eI[src_index] <- DF$eI; eQ[src_index] <- DF$eQ; eU[src_index] <- DF$eU
-	} else {
-		eI[src_index] <- sd(DF$eI); eQ[src_index] <- sd(DF$eQ); eU[src_index] <- sd(DF$eU)
-	}
-}
-polDF <- data.frame( Src=as.character(sourceList), I=medI, eI = eI, Q=medQ, eQ = eQ, U=medU, eU = eU, P=sqrt(medQ^2 + medU^2), eP=sqrt(eQ^2 + eU^2)/medI, p=100.0*sqrt(medQ^2 + medU^2)/medI, EVPA=90.0*atan2(medU, medQ)/pi )
-polDF <- polDF[order(polDF$P, decreasing=T),]
-rownames(polDF) <- c(1:nrow(polDF))
-save(polDF, file='Pol.Rdata')
-
-sourceList <- sort(polDF$Src[grep('^J[0-9]', polDF$Src)])
+textDF$Src <- sprintf('%10s ', textDF$Src)
+write.table(format(textDF, digits=4), 'amapola.txt', sep='\t', quote=F, col.names=T, row.names=F)
+#-------- fiter Band-3 upper and lower sidebands
+textDF <- textDF[((textDF$Freq != standardFreq[[3]][1]) & (textDF$Freq != standardFreq[[3]][3])),]
+textDF$Band <- getBand(textDF$File)
 #-------- Plot time-seried flux densities
+textDF$Src <- trimws(textDF$Src)
+sourceList <- unique(textDF$Src)
+sourceList <- sourceList[grep('^J[0-9]',sourceList)]
+sourceList <- sourceList[order(sourceList)]
 pdf('AMAPOLA.pdf', width=8, height=11)
 par.old <- par(no.readonly=TRUE)
 par(mfrow=c(3,1), oma=c(0, 0, 4, 0), mar=c(4,4,4,4))
-cat('Source     I [Jy]   Q [Jy]   U [Jy]   %Pol   EVPA [deg]\n')
 labels <- c("B1",        "B3",        "B4",        "B6",        "B7",        "> B7")
-colors <- c("#FF000000", "#E020003F", "#8080003F", "#00C0803F", "#0000FF3F", "#FFFFFF")
-threshU <-c(41.0,        98.0,        200.0,       280.0,       380.0,       700.0)
-threshL <-c(39.0,        97.0,        120.0,       200.0,       280.0,       380.0)
+colors <- c("#FF80003F", "#E020003F", "#8080003F", "#00C0803F", "#0000FF3F", "#0000003F")
+#threshU <-c(41.0,        98.0,        200.0,       280.0,       380.0,       700.0)
+#threshL <-c(39.0,        97.0,        120.0,       200.0,       280.0,       380.0)
 plotXrange <- range(FLDF$Date)
+bandList <- unique(textDF$Band)
+bandIndex <- c(1,0,2,3,0,4,5,6,0)
 for(sourceName in sourceList){
-	DF <- FLDF[FLDF$Src == sourceName,]
-	pDF <- polDF[polDF$Src == sourceName,]
-	cat(sprintf("%10s %6.2f %6.2f %6.2f %6.2f %6.2f\n", pDF$Src, pDF$I, pDF$Q, pDF$U, pDF$p, pDF$EVPA))
+	DF <- textDF[textDF$Src == sourceName,]
     #-------- Coloring by frequency
     DF$color_vector <- rep("#000000FF", nrow(DF))
-    for(color_index in 1:length(labels)){
-        band_index <- which( (DF$Freq > threshL[color_index]) & (DF$Freq < threshU[color_index]) )
-        DF$color_vector[band_index] <- colors[color_index]
+    for(band in bandList){
+        if(nrow(DF[DF$Band == band,]) == 0){ next }
+        DF[DF$Band == band,]$color_vector <- colors[bandIndex[band]]
     }
+    DF <- DF[DF$color_vector != "#000000FF",]
 	#-------- Plot Stokes I
 	plot(DF$Date, DF$I, type='n', xlab='Date', ylab='Stokes I [Jy]', main='Total Flux Density', xlim=as.POSIXct(plotXrange), ylim=c(0, 1.2*max(DF$I)))
 	arrows(as.numeric(DF$Date), DF$I - DF$eI, as.numeric(DF$Date), DF$I + DF$eI, angle=90, length=0.0, col=DF$color_vector)
@@ -231,6 +117,57 @@ for(sourceName in sourceList){
 	points(DF$Date, DF$EVPA*180/pi, pch=20, col=DF$color_vector)
     axis.Date(1, at=seq(min(DF$Date), max(DF$Date), "months"), format="%Y-%m", cex=0.5)
 	mtext(side = 3, line=1, outer=T, text = sourceName, cex=2)
+
+    plot_I <- plot_ly(data=DF[DF$Band == bandList[1],], x=~Date, y=~I, type="scatter", mode="markers", color=paste("I", freqLabel[1]), colors=bandColor, error_y = list(array=~eI, thickness=1, width=0))
+    plot_P <- plot_ly(data=DF[DF$Band == bandList[1],], x=~Date, y=~P, type="scatter", mode="markers", color=paste("P",freqLabel[1]), colors=bandColor, error_y = list(symmetric=FALSE, array=~errU, arrayminus=~errL, thickness=1, width=0))
+    plot_A <- plot_ly(data=DF[DF$Band == bandList[1],], x=~Date, y=~EVPA*180/pi, type="scatter", mode="markers", color=paste("EVPA",freqLabel[1]), colors=bandColor, error_y = list(array=~eEVPA*180/pi, thickness=1, width=0))
+    allPlot <- subplot(plot_I, plot_P, plot_A, nrows=3, shareX=T, titleY=T)
+    htmlFile <- sprintf("%s.flux.html", sourceList[src_index])
+    htmlwidgets::saveWidget(allPlot, htmlFile, selfcontained=FALSE )
+    rm(plot_I); rm(plot_P); rm(plot_A); rm(allPlot); rm(htmlFile)
 }
 par(par.old)
 dev.off()
+text_sd <- sprintf('cp -r %s.flux_files common.flux_files', sourceList[1])
+system(text_sd)
+system('rm -rf J*.flux_files')
+for(sourceName in sourceList){
+    text_sd <- sprintf('ln -s common.flux_files %s.flux_files', sourceName)
+    system(text_sd)
+}
+if(0){
+#-------- Time-series HTML
+bandColor <- brewer.pal(numFreq, "Dark2")
+for(sourceName in sourceList){
+	DF <- textDF[textDF$Src == sourceName,]
+    plot_I <- plot_ly(data=DF[DF$Band == bandList[1],], x=~Date, y=~I, type="scatter", mode="markers", color=paste("I", freqLabel[1]), colors=bandColor, error_y = list(array=~eI, thickness=1, width=0))
+    for(freq_index in 2:numFreq){
+        if(nrow(DF[DF$Band == bandList[freq_index],]) > 0){ plot_I <- add_trace(plot_I, data=DF[DF$Band == bandList[freq_index],], color=paste("I", freqLabel[freq_index]))}
+    }
+    plot_I <- layout(plot_I, xaxis=list(showgrid=T, title='Date', range=c(min(DF$Date)-86400, max(DF$Date)+86400)), yaxis=list(showgrid=T, title='Stokes I [Jy]',rangemode='tozero', hoverformat='.3f'), title=sourceList[src_index])
+
+    plot_P <- plot_ly(data=DF[DF$Band == bandList[1],], x=~Date, y=~P, type="scatter", mode="markers", color=paste("P",freqLabel[1]), colors=bandColor, error_y = list(symmetric=FALSE, array=~errU, arrayminus=~errL, thickness=1, width=0))
+    for(freq_index in 2:numFreq){
+        if(nrow(DF[DF$Band == bandList[freq_index],]) > 0){ plot_P <- add_trace(plot_P, data=DF[DF$Band == bandList[freq_index],], color=paste("P",freqLabel[freq_index]))}
+    }
+    plot_P <- layout(plot_P, xaxis=list(showgrid=T, title='Date', range=c(min(DF$Date)-86400, max(DF$Date)+86400)), yaxis=list(showgrid=T, title='Polarized Flux [Jy]',rangemode='tozero', hoverformat='.3f'))
+
+    plot_A <- plot_ly(data=DF[DF$Band == bandList[1],], x=~Date, y=~EVPA*180/pi, type="scatter", mode="markers", color=paste("EVPA",freqLabel[1]), colors=bandColor, error_y = list(array=~eEVPA*180/pi, thickness=1, width=0))
+    for(freq_index in 2:numFreq){
+        if(nrow(DF[DF$Band == bandList[freq_index],]) > 0){ plot_A <- add_trace(plot_A, data=DF[DF$Band == bandList[freq_index],], color=paste("EVPA",freqLabel[freq_index]))}
+    }
+    plot_A <- layout(plot_A, xaxis=list(showgrid=T, title='Date', range=c(min(DF$Date)-86400, max(DF$Date)+86400)), yaxis=list(showgrid=T, title='EVPA [deg]',range=c(-91,91), hoverformat='.1f'))
+
+    allPlot <- subplot(plot_I, plot_P, plot_A, nrows=3, shareX=T, titleY=T)
+    htmlFile <- sprintf("%s.flux.html", sourceList[src_index])
+    htmlwidgets::saveWidget(allPlot, htmlFile, selfcontained=FALSE )
+    rm(plot_I); rm(plot_P); rm(plot_A); rm(allPlot); rm(htmlFile)
+}
+text_sd <- sprintf('cp -r %s.flux_files common.flux_files', sourceList[1])
+system(text_sd)
+system('rm -rf J*.flux_files')
+for(src_index in 1:numSrc){
+    text_sd <- sprintf('ln -s common.flux_files %s.flux_files', sourceList[src_index])
+    system(text_sd)
+}
+}
