798e07205411a351f7ff1921cc61b516fc31eb57
kamenoseiji <seiji.kameno@alma.cl>
2025-09-23 20:11:55 +0000

    modified:   polCalCSV.R

--- polCalCSV.R
+++ polCalCSV.R
@@ -2,6 +2,8 @@
 #         Band1      2     3      4     5      6     7      8      9   10
 BandPA <- c(45.0, -45.0, 80.0, -80.0, 45.0, -45.0, 36.45, 90.0, -90.0, 0.0)*pi/180
 BandFreq <- c(43.0, 75.0, 97.5, 132.0, 183.0, 233.0, 343.5, 400.0, 650.0, 800.0)
+Pthresh  <- c(0.06, 0.07, 0.07, 0.08, 0.10, 0.09, 0.13, 0.3, 1.1, 1.7)     # thresholds for polarized flux
+SECPERDAY <- 86400
 ALMA_LAT <- -23.029* pi/180.0   # [radian]
 ALMA_LONG <- -67.755* pi/180.0  # [radian]
 cos_phi <- cos(ALMA_LAT)
@@ -11,7 +13,6 @@ minSinEL <- sin(30/180*pi)
 hourPerRad <- 12/pi		# radian to hour angle conversion
 sessionDuration <- 3/hourPerRad	# 3 hours in [rad]
 pointingDuration <- 0.1/hourPerRad	# 0.1 hourss in [rad]
-Pthresh <- 0.05    # polarized flux > 50 mJy
 DateRange <- 60    # 60-day window
 #-------- # cos(hour angle) when it passes the given EL
 EL_HA <- function(sinEL, dec){
@@ -22,13 +23,14 @@ EL_HA <- function(sinEL, dec){
 estimateIQUV <- function(DF, refFreq){
     DF$relFreq <- DF$Freq / refFreq
     timeWeightSoftening <- 5* 86400 # 5-day softening
-    if(diff(range(DF$Freq)) < 100 ){ return( data.frame( Src=DF$Src[1], I=0.0, Q=0.0, U=0.0, V=0.0, P=0.0, EVPA=0.0))}
-    if((length(unique(DF$relTime)) < 4) | (max(DF$relTime) < -15* 86400)){
-        fitI <- lm(formula=log(I) ~ log(relFreq), data=DF, weight=(I / eI)^2 * (timeWeightSoftening / abs(relTime + timeWeightSoftening)) )
-	    fitP <- lm(formula=log(P) ~ log(relFreq), data=DF, weight=(DF$P/DF$eP)^2 * (timeWeightSoftening / abs(relTime + timeWeightSoftening)))
+    if( (max(DF$relFreq) < 0.65) | (max(DF$relFreq) / min(DF$relFreq) < 2.0 )){ return( data.frame( Src=DF$Src[1], I=0.0, Q=0.0, U=0.0, V=0.0, P=0.0, EVPA=0.0))}
+    DF$timeFreqDeparture <- (abs(DF$relTime) + timeWeightSoftening) * (1.0 + abs(DF$relFreq - 1))
+    if( (diff(range(DF$relTime)) < 0.25* DateRange* SECPERDAY) | (max(DF$relTime) < -0.25* SECPERDAY)){      # small number of data
+        fitI <- lm(formula=log(I) ~ log(relFreq), data=DF, weight=(I / eI) * (timeWeightSoftening / timeFreqDeparture))
+	    fitP <- lm(formula=log(P) ~ log(relFreq), data=DF, weight=(P / eP) * (timeWeightSoftening / timeFreqDeparture))
     } else {
-        fitI <- lm(formula=log(I) ~ relTime + log(relFreq), data=DF, weight=(I / eI)^2 * (timeWeightSoftening / abs(relTime + timeWeightSoftening)) )
-	    fitP <- lm(formula=log(P) ~ relTime + log(relFreq), data=DF, weight=(P/eP)^2 * (timeWeightSoftening / abs(relTime + timeWeightSoftening)))
+        fitI <- lm(formula=log(I) ~ log(relFreq) + relTime, data=DF, weight=(I / eI) * (timeWeightSoftening / timeFreqDeparture))
+	    fitP <- lm(formula=log(P) ~ log(relFreq) + relTime, data=DF, weight=(P / eP) * (timeWeightSoftening / timeFreqDeparture))
     }
     weight <- 1.0/(abs(DF$eEVPA) * sqrt(DF$eQ^2 + DF$eU^2)* abs(log(DF$relFreq) + 1.0)^2 * (timeWeightSoftening / abs(DF$relTime + timeWeightSoftening)))
     Twiddle <- sum( weight* exp((0.0 + 2.0i)*DF$EVPA) ) / sum(weight); Twiddle <- Twiddle/abs(Twiddle)
@@ -69,7 +71,7 @@ for(band in seq(1, 7)){
         srcDF[srcDF$Src == src,]$RA  <- pi* (60.0* as.numeric(substring(src, 2, 3)) + as.numeric(substring(src, 4, 5))) / 720.0
         srcDF[srcDF$Src == src,]$DEC <- pi* sign(as.numeric(substring(src, 6, 10)))* (as.numeric(substring(src, 7, 8)) + as.numeric(substring(src, 9, 10))/60.0) / 180.0
     }
-    srcDF <- srcDF[srcDF$P > Pthresh,]  # filter by polarized flux
+    srcDF <- srcDF[srcDF$P > Pthresh[band],]  # filter by polarized flux
     srcDF <- srcDF[srcDF$DEC < ALMA_LAT + pi/3,]  # max EL > 30 deg
     srcDF <- srcDF[srcDF$DEC > ALMA_LAT - pi/3,]  # max EL > 30 deg
     srcDF <- srcDF[abs(srcDF$DEC - ALMA_LAT) > 3.0* pi / 180,]  # avoid zenith passage
@@ -83,7 +85,7 @@ for(band in seq(1, 7)){
         sin_dec <- sin(srcDF$DEC[index])
         PA <- atan2(sinHA, sin_phi*cos_dec/cos_phi - sin_dec*cosHA) + BandPA[band]
         XYcorr <- srcDF$U[index]* cos(2.0*PA) - srcDF$Q[index]* sin(2.0*PA)
-		XY_overthresh <- HA[which(abs(XYcorr) > Pthresh)]		# HA to obtain |XY| > Pthresh
+		XY_overthresh <- HA[which(abs(XYcorr) > Pthresh[band])]		# HA to obtain |XY| > Pthresh
         XY_intercepts <- HA[which(diff(sign(XYcorr)) != 0)]     # hour angles of sign transition
 		if(length(XY_overthresh) * length(XY_intercepts) == 0){
             srcDF$P[index] <- NA
@@ -101,7 +103,7 @@ for(band in seq(1, 7)){
 		png(pngFile)
 		plot(HA*hourPerRad, XYcorr, type='l', col='red', xlab='Hour Angle [h]', ylab='XY correlation [Jy]', main=sprintf('%s Band%d', srcDF$Src[index], band))
 		grid(nx=NULL, ny=NULL, lty=2, col='gray', lwd=1)
-		abline(h=Pthresh, lty=2); abline(h=-Pthresh, lty=2); abline(v=hourPerRad* XY_intercepts)
+		abline(h=Pthresh[band], lty=2); abline(h=-Pthresh[band], lty=2); abline(v=hourPerRad* XY_intercepts)
 		lines(c(HA_min, HA_max)*hourPerRad, c(0,0), lwd=4, col='blue')
 		dev.off()
 		srcDF$png[index] <- pngFile
